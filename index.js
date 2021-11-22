f8871a334f923c68465958c8321614bd51f8683c
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2021-11-22 13:22:39 -0600

    Coded another GET endpoint but this time to request a specific song. Both GET endpoints were successfullytested on Postman

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.listen(3000, function () {
     console.log("Server Started. Listening on Port 3000.");
 });
 
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
