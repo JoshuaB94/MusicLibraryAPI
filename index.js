3bf1c73d3a840b84bfe45b50a7470aef5d5a84b9
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2021-11-22 13:31:53 -0600

    Changed Port from 3000 to 1000 for purpose of running both App and API when it time to connect to each other + Coded a POST endpoint so a new song can be added to core dataSet.

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const repoContext = require('./repository/repository-wrapper');
 
 const app = express();
 
-app.listen(3000, function () {
-    console.log("Server Started. Listening on Port 3000.");
+app.listen(1000, function () {
+    console.log("Server Started. Listening on Port 1000.");
 });
 
 app.get('/api/songs', (req, res) => {
     const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
+});
+
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
 });
